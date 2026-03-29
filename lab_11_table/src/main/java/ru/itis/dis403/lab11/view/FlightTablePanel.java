27e9b162ac00da728f6acaf95a3a0564c0537614
ALEKSANDER ISAEV <topklopxip@gmail.com>
2026-03-29 20:18:27 +0300

    .

--- lab_11_table/src/main/java/ru/itis/dis403/lab11/view/FlightTablePanel.java
+++ lab_11_table/src/main/java/ru/itis/dis403/lab11/view/FlightTablePanel.java
@@ -48,7 +48,7 @@ public class FlightTablePanel extends JPanel {
                 "Куда"
         };
 
-        // модель таблицы:FEWFWEF
+        // модель таблицы:FEWFWEFGEGEG
         // columnsNames — заголовкFFFпппппппп
         // 0 — изначально нет строкаааgrgr
         tableModel = new DefaultTableModel(columnsNames, 0) {
